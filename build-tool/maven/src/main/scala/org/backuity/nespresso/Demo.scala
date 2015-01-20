02765a8a4340f0610fc212f57ee2b85c420bf85e
Bruno Bieth <biethb@gmail.com>
2015-01-20 21:16:21 +0100

    maven archetype

---
+++ build-tool/maven/src/main/scala/org/backuity/nespresso/Demo.scala
@@ -0,0 +1,8 @@
+package org.backuity.nespresso
+
+object Demo {
+
+  def main(args: Array[String]) {
+    println("Hello this is the Maven based Demo")
+  }
+}
---
+++ build-tool/maven/src/test/scala/org/backuity/nespresso/DemoTest.scala
@@ -0,0 +1,11 @@
+package org.backuity.nespresso
+
+import org.junit.Test
+
+class DemoTest {
+
+  @Test
+  def testSomeStuff(): Unit = {
+    println("Ok, tested just fine")
+  }
+}
